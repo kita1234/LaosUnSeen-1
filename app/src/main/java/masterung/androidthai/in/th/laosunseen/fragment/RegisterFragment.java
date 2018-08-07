2a480bf59133eb16f1d18a96556f47bdf663c6b7
kita <kita_secret@hotmail.com>
2018-08-07 16:18:59 +0700

    Toolbar succesful

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -1,5 +1,9 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
@@ -15,12 +21,67 @@ import masterung.androidthai.in.th.laosunseen.R;
 
 public class RegisterFragment extends Fragment{
 
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
+
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
 
+
+//     Photo Controller
+        photoController();
+
+    }// Main Class
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 400, 300, true);
+                imageView.setImageBitmap(bitmap1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(),"please choose photo",Toast.LENGTH_SHORT).show();
+
+        }// show message for confirm user know that
+
+
+
+
+    }
+
+    private void photoController() {
+         imageView = getView().findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Please Choose app"),1);
+
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -34,8 +95,8 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
 
             }
-        });
-    }// Main Class
+        });// new method from main method
+    }
 
     @Nullable
     @Override
